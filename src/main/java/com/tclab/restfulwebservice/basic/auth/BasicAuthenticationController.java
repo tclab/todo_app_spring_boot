119e32ebd3e3e670296edd12352e5bcc7bd4cf04
judtoro <juan.toro@globant.com>
2020-05-25 09:28:25 -0500

    Basic authentication refactor

---
+++ src/main/java/com/tclab/restfulwebservice/basic/auth/BasicAuthenticationController.java
@@ -0,0 +1,20 @@
+package com.tclab.restfulwebservice.basic.auth;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+// to allowd cross calls we use CrossOrigin annotation
+@CrossOrigin(origins = "http://localhost:4200")
+public class BasicAuthenticationController {
+
+  @GetMapping(path = "/basicauth")
+  public AuthenticationBean helloWorldBean() {
+    return new AuthenticationBean("You are authenticated");
+  }
+
+
+
+}
